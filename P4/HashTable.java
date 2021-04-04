3c5e4713297f2d0d7a67fa994265aa5c5da65609
Ben Scholar <bbs27@case.edu>
2021-04-04 11:16:23 -0400

    changed from arraylists to array, got rid of most optionals

--- P4/HashTable.java
+++ P4/HashTable.java
@@ -1,34 +1,33 @@
 
 import java.util.Optional;
-import java.util.ArrayList;
 
 class HashTable {
-    // constants
+    // facctor to expand buckets by during rehash
     private static final float kExpansionFactor = 2.0f;
+    // max load factor rehash threshold
+    // rehash if load factor is above this value
     private static final float kMaxLoadingThreshold = 0.75f;
+    // min load factor rehash threshold
+    // rehash if load factor is below this value
     private static final float kMinLoadingThreshold = 0.1f;
 
-    // member items
 
     // Number of buckets in the hashtable
     private int mCapacity;
     // Number of items in the table
     private int mItems;
-    // buckets
-    private ArrayList<Optional<Entry>> mData;
+    // buckets 
+    private Entry[] mBuckets;
     // load factor
     private float mLoadFactor;
-        
+
     public HashTable() {
         mItems = 0;
         mCapacity = 1;
-        
+
         // allocate buckets as empty
-        mData = new ArrayList<Optional<Entry>>(mCapacity);
-        mData.ensureCapacity(mCapacity);
-        for (int i = 0; i < mCapacity; ++i)
-            mData.add(Optional.empty());
-        
+        mBuckets = new Entry[mCapacity];
+
         // load factor starts at 0
         mLoadFactor = 0f;
     }
@@ -37,9 +36,12 @@ class HashTable {
     private void checkForRehash() {
         mLoadFactor = (float) mItems / (float) mCapacity;
 
+        // check if the current loadfactor is between our desired bounds, if it
+        // isnt, we will need to rehash
         if (mLoadFactor > kMaxLoadingThreshold || mLoadFactor < kMinLoadingThreshold) {
+            // store the old data and capacity for future operations
             final int oldCapacity = mCapacity;
-            ArrayList<Optional<Entry>> oldData = mData;
+            final Entry[] oldData = mBuckets;
 
             if (mLoadFactor > kMaxLoadingThreshold) {
                 mCapacity = (int) Math.ceil(oldCapacity * kExpansionFactor);
@@ -47,127 +49,174 @@ class HashTable {
                 mCapacity = (int) Math.ceil(oldCapacity / kExpansionFactor);
             }
 
-            mData = new ArrayList<>(mCapacity);
+            // allocate new storage
+            mBuckets = new Entry[mCapacity];
 
-            // O(2*n)
-            for(int i = 0; i < mCapacity; ++i) {
-                mData.add(Optional.empty());
-            }
-
-            for(int i = 0; i < oldCapacity; ++i) {
-                Optional<Entry> entry = oldData.get(i);
-                while(entry.isPresent()) {
-                    // O(n)
-                    insert(entry.get().key, entry.get().frequency, false);
-                    entry = entry.get().next;
+            // O(n): insert every element from the old buckets into
+            // the new buckets with the new hash function.
+            for (int i = 0; i < oldCapacity; ++i) {
+                Entry entry = oldData[i];
+                while (entry != null) {
+                    // O(1)
+                    insert(entry.key, entry.frequency, false);
+                    entry = entry.next;
                 }
             }
-
-            oldData.clear();
         }
     }
 
-    //O(1)
-    private int getHash(char c) {
+    // O(1)
+    private int getHash(final char c) {
         return Math.abs(Character.getNumericValue(c) % mCapacity);
     }
-
-    public boolean insert(char c) {
+    
+    // OVerride of the insert, sets rehash to false and teh frequnecy to 1
+    public boolean insert(final char c) {
         return insert(c, 1);
     }
+    
 
-    public boolean insert(char c, long freq) {
+    // Override for the function below, sets `rehash` to true
+    public boolean insert(final char c, final long freq) {
         return insert(c, freq, true);
     }
 
     // O(1) average, O(n) worst case
-    private boolean insert(char c, long freq, boolean rehash) {
+    // the `rehash` flag denotes whether the method will increment the number of items
+    // and check for a rehash, this is set to false when a rehash is occuring to prevent unnessesary recursrsive
+    // calls to the `checkForRehash` method.
+    // returns true if insertion is successfull
+    private boolean insert(final char c, final long freq, final boolean rehash) {
         final int hash = getHash(c);
 
-        Optional<Entry> parent = Optional.empty();
-        Optional<Entry> entry = mData.get(hash);
-
-        while (entry.isPresent()) {
-            if (entry.get().key == c)
+        Entry parent = null;
+        Entry entry = mBuckets[hash];
+    
+        // find the next available spot in the bucket
+        while (entry != null) {
+            // return false if this key already exists
+            if (entry.key == c)
                 return false;
             parent = entry;
-            entry = entry.get().next;
+            entry = entry.next;
         }
-
-        final Optional<Entry> newEntry = Optional.of(new Entry(c, freq));
-
-        if (parent.isEmpty()) {
-            mData.set(hash, newEntry);
+        
+        // declare the new entry
+        final Entry newEntry = new Entry(c, freq);
+        
+        // if the parent is null, we know that that bucket is empty,
+        // so we can set the 'root' entry to the new entry
+        // otherwise, append it to the back of the list
+        if (parent == null) {
+            mBuckets[hash] = newEntry;
         } else {
-            parent.get().next = newEntry;
+            parent.next = newEntry;
         }
-
-        if(rehash) {
+        
+        // increment item count and rehash if the rehash flag is set
+        if (rehash) {
             mItems++;
             checkForRehash();
         }
 
         return true;
     }
-
-    public boolean delete(char c) {
+    
+    // O(1) average, O(n) worstcase
+    public boolean delete(final char c) {
+        // get the hash
         final int hash = getHash(c);
+    
+        // initialize cursors
+        Entry parent = null;
+        Entry entry = mBuckets[hash];
 
-        Optional<Entry> parent = Optional.empty();
-        Optional<Entry> entry = mData.get(hash);
-
-        while (entry.isPresent()) {
-            if (entry.get().key == c) {
-                // delete
-
-                if (parent.isEmpty())
-                    mData.set(hash, entry.get().next);
+        while (entry != null) {
+            // if we find a matching key, delete the entry
+            if (entry.key == c) {
+                // if the parent is null, we know this is the root entry, so just set
+                // the root entry to the next entry (which could be null, but this is handled implicitly)
+                // otherwise delete normally
+                if (parent == null)
+                    mBuckets[hash] = entry.next;
                 else {
-                    parent.get().next = entry.get().next;
+                    parent.next = entry.next;
                 }
-
-                mItems--;
                 
+                // decrement item count
+                // and check for rehash
+                mItems--;
                 checkForRehash();
 
                 return true;
             }
             parent = entry;
-            entry = entry.get().next;
+            entry = entry.next;
         }
 
+        // return false if we do not find an entry with a matching key 
         return false;
     }
-    
-    // O(1) average, O(n) worstcase
-    public Optional<Entry> search(char c) {
+
+    // O(1) average
+    public Optional<Entry> search(final char c) {
+        // get hash
         final int hash = getHash(c);
-
-        Optional<Entry> entry = mData.get(hash);
-        while (entry.isPresent() && entry.get().key != c) {
-            entry = entry.get().next;
+        
+        // search the bucket for a matching key
+        Entry entry = mBuckets[hash];
+        while (entry != null && entry.key != c) {
+            entry = entry.next;
         }
-
-        return entry;
+            
+        // return optional empty if we do not find a valid entry
+        return entry != null ? Optional.of(entry) : Optional.empty();
     }
-    
+
     // O(1)
     public boolean isEmpty() {
         return mItems == 0;
     }
 
+    // Entry class
+    // contains the key and frequency of each char
+    // also contains a ptr to the next entry in the bucket
+    // should be fairly self explanatory
     public class Entry {
-        private Entry(char key, long freq) {
+        private Entry(final char key, final long freq) {
             this.key = key;
             this.frequency = freq;
-            this.next = Optional.empty();
+            this.next = null;
         }
 
-        public final char key;
-        public long frequency;
+        private final char key;
+        private long frequency;
+        private Entry next;
 
-        private Optional<Entry> next;
+        public char getKey() {
+            return key;
+        }
 
+        public long getFrequency() {
+            return frequency;
+        }
+
+        public void setFrequency(final long frequency) {
+            this.frequency = frequency;
+        }
+
+        public void incrementFrequency() {
+            this.frequency++;
+        }
+
+        public void decrementFrequency() {
+            this.frequency--;
+        }
+
+        @Override
+        public String toString() {
+            return "Entry [frequency=" + frequency + ", key=" + key + "]";
+        }
 
     }
 }
--- P4/Main.java
+++ P4/Main.java
@@ -2,40 +2,68 @@
 import java.util.Optional;
 
 class Main {
-
+    
+    // Time complexity: O(n) where n is the number of non-whitespace characters
+    // in strings s1 and s2 given they have the same length.
     public static boolean anagrams(final String s1, final String s2) {
-        HashTable table = new HashTable();
-
+        
+        // get the character arrays of both strings after removing
+        // spaces and setting all characters to lowercase.
+        // These are most likely O(n) operations(?)
         final char[] s1Chars = s1.replace(" ", "").toLowerCase().toCharArray();
         final char[] s2Chars = s2.replace(" ", "").toLowerCase().toCharArray();
+    
 
-        for(final char c : s1Chars) {
-            Optional<HashTable.Entry> entry = table.search(c);
+        // If there are a different ammount of characters in each string,
+        // then the strings cannot be anagrams
+        // this saves us some CPU and allocations
+        if(s1Chars.length != s2Chars.length)
+            return false;
+        
+        // allocate the hashtable
+        HashTable table = new HashTable();
+        
+        // for every character in the first string's array
+        // add the character to the table if it doesnt exist yet,
+        // else, increment the frequency of that character
+        // O(n)
+        for(final char c : s1Chars) { 
+            Optional<HashTable.Entry> entry = table.search(c); // O(1) avg
             if(entry.isPresent()) {
-                entry.get().frequency++; 
+                entry.get().incrementFrequency(); 
                 continue;
             }
-            table.insert(c);
+            table.insert(c); // O(n) worst
         }
+    
 
+        // next, for every character in the second string, 
+        // search for that character in the table, if it doesnt exist as a key,
+        // return false, else decrement the frequency and delete if the frequency is 0
+        // O(n)
         for(final char c: s2Chars) {
-            Optional<HashTable.Entry> entry = table.search(c);
+            Optional<HashTable.Entry> entry = table.search(c); // O(1)
             if(entry.isEmpty()) return false;
             else {
-                entry.get().frequency--;
+                entry.get().decrementFrequency();
 
-                if(entry.get().frequency == 0) {
-                    table.delete(c);
+                if(entry.get().getFrequency() == 0) {
+                    table.delete(c); // O(1) average, O(n) worst case
                 }
             }
         }
         
+        // return true if the table is empty, which signifies that they
+        // are anagrams
         return table.isEmpty();
     }
 
     public static void main(String... args) {
-        String s1 = "cdeezs";
-        String s2 = "eezd";
+        /* String s1 = "cdeezs"; */
+        /* String s2 = "eezd"; */
+        String s1 = "deezs 12345";
+        String s2 = "eezds 54321";
+
         System.out.println(anagrams(s1, s2));
     }
 
